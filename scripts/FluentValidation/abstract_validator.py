ed2389778ad6fd542f877a1d585855ee98681047
phzamora <p.hzamora@icloud.com>
2023-12-11 14:58:35 +0100

    chore: reorganized functions and created regions

--- scripts/FluentValidation/abstract_validator.py
+++ scripts/FluentValidation/abstract_validator.py
@@ -144,16 +144,16 @@ class RuleBuilder[T,TProperty](IRuleBuilder,IRuleBuilderInternal): # no implemen
         return self._rule
         
 
+
 class AbstractValidator[T](ABC):
+    #region constructor
     def __init__(self) -> None:
         self._classLevelCascadeMode:Callable[[],CascadeMode] = lambda : ValidatorOptions.Global.DefaultClassLevelCascadeMode
         self._ruleLevelCascadeMode:Callable[[],CascadeMode] = lambda : ValidatorOptions.Global.DefaultRuleLevelCascadeMode
         self._rules:list[PropertyRule] = []
+    #endregion
 
-    def validate(self, instance:T)->ValidationResult:
-        return self.internal_validate(ValidationContext(instance))
-
-
+    #region Private methods
     def internal_validate(self, context:ValidationContext)->ValidationResult:
         result:ValidationResult = ValidationResult(errors=context.Failures)
         for rule in self._rules:
@@ -164,10 +164,21 @@ class AbstractValidator[T](ABC):
         # self.SetExecutedRuleSets(result,context)
         return result
 
+    def SetExecutedRuleSets(self, result:ValidationResult, context:ValidationContext[T]):...
+        #result.RuleSetExecuted = RulesetValidatorSelector.DefaultRuleSetNameInArray
+    #endregion 
+    
+    #region Public methods
+    def validate(self, instance:T)->ValidationResult:
+        return self.internal_validate(ValidationContext(instance))
+
     def RuleFor[TProperty](self,func:Callable[[T],TProperty])->IRuleBuilder[T,TProperty]: #IRuleBuilderInitial[T,TProperty]:
         rule:PropertyRule[T,TProperty] = PropertyRule.create(func, lambda: self.RuleLevelCascadeMode)
         self._rules.append(rule)
         return RuleBuilder(rule,self)
+    #endregion
+
+    #region Properties
     @property
     def ClassLevelCascadeMode(self)->CascadeMode:
         return self._classLevelCascadeMode()
@@ -180,3 +191,4 @@ class AbstractValidator[T](ABC):
         return self._ruleLevelCascadeMode()
     @RuleLevelCascadeMode.setter
     def RuleLevelCascadeMode(self, value): self._ruleLevelCascadeMode = lambda: value
+    #endregion

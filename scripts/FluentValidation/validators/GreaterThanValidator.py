11056400fd62091b2d35473b9228e465c15f4a41
phzamora <p.hzamora@icloud.com>
2023-12-13 13:40:51 +0100

    feat: added GreaterThanValidator

--- scripts/FluentValidation/validators/GreaterThanValidator.py
+++ scripts/FluentValidation/validators/GreaterThanValidator.py
@@ -1,41 +1,26 @@
 from typing import Callable, overload, override
 from stc.common.scripts.FluentValidation.validators.AbstractComparisonValidator import AbstractComparisonValidator, Comparison
 
-class MemberInfo: ... #    BORRAR clase cuando acabe
-
 
 class GreaterThanValidator[T, TProperty](AbstractComparisonValidator[T, TProperty]):
-
-
     @overload
     def __init__(self, value:TProperty):...
     @overload
-    def __init__(self, valueToCompareFunc: Callable[[T], TProperty], member:MemberInfo, memberDisplayName:str): ... 
+    def __init__(self, valueToCompareFunc: Callable[[T], TProperty], memberDisplayName:str): ... 
     @overload
-    def __init__(self, valueToCompareFunc:Callable[[T], tuple[bool,TProperty]], member:MemberInfo, memberDisplayName:str):...
-    
-    
+    def __init__(self, valueToCompareFunc:Callable[[T], tuple[bool,TProperty]], memberDisplayName:str):...
     
     def __init__(self
             , value= None
             , valueToCompareFunc= None
-            , member= None
             , memberDisplayName= None
             ):
         super().__init__(
-              valueToCompareFunc= valueToCompareFunc
-            , member= member
+              valueToCompareFunc= valueToCompareFunc    
             , memberDisplayName= memberDisplayName
             , value= value
                 )
-
-    @override
-    def is_valid(self, value:TProperty, valueToCompare:TProperty):
-        if valueToCompare is None:
-            return False
-
-        return value > valueToCompare 
-        # return value.CompareTo(valueToCompare) > 0
     
     @override
-    def Comparison(self)->Comparison: Comparison.GreaterThan
+    @property
+    def Comparison(self)->Comparison: return Comparison.GreaterThan

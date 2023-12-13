9724ba02ad0b134e49bb8b8423f5ede5a9c31d89
phzamora <p.hzamora@icloud.com>
2023-12-13 13:27:30 +0100

    refactor: deleted classes  implementation

--- scripts/FluentValidation/abstract_validator.py
+++ scripts/FluentValidation/abstract_validator.py
@@ -1,148 +1,14 @@
 from abc import ABC
+from typing import Callable
 
-from typing import Callable, List, TypeVar
-import dis
-
-from IValidationRule import IValidationRule
-from internal.MessageBuilderContext import MessageBuilderContext
-from internal.RuleComponent import RuleComponent
-from results.ValidationFailure import ValidationFailure
-
-
-from validators.IpropertyValidator import IPropertyValidator
 from results.ValidationResult import ValidationResult
 from IValidationContext import ValidationContext
 from IValidationRule import *
 from syntax import *
+from stc.common.scripts.FluentValidation.internal.PropertyRule import PropertyRule
+from stc.common.scripts.FluentValidation.internal.RuleBuilder import RuleBuilder
+
 from DefaultValidatorExtensions import CascadeMode, ValidatorOptions
-
-
-
-
-TAbstractValidator = TypeVar("TAbstractValidator",bound="AbstractValidator")
-TRuleBuilder = TypeVar("TRuleBuilder",bound="RuleBuilder")
-TPropertyRule = TypeVar("TPropertyRule",bound="PropertyRule")
-
-
-
-class RuleBase[T,TProperty,TValue](IValidationRule[T,TValue]):
-    def __init__(self,propertyFunc:Callable[[T],TProperty], cascadeModeThunk:Callable[[],CascadeMode], type_to_validate:type):
-        self._PropertyFunc = propertyFunc
-        self._type_to_validate = type_to_validate
-        self._cascadeModeThunk:Callable[[],CascadeMode] = cascadeModeThunk
-        self._components:List[RuleComponent[T, TProperty]] = []
-        self._propertyName:str = {x.opname:x.argval for x in dis.Bytecode(propertyFunc)}["LOAD_ATTR"]
-        self._displayName:str = self._propertyName
-
-    @property
-    def PropertyFunc(self)->Callable[[T],TProperty]: return self._PropertyFunc
-    @property
-    def TypeToValidate(self): return self._type_to_validate
-
-    @property
-    def Components(self): return self._components
-
-    @property
-    def PropertyName(self): return self._propertyName
-    
-    @property
-    def displayName(self): return self._displayName
-
-    @property
-    def Current(self)->IRuleComponent: return self._components[-1]
-
-    @property
-    def MessageBuilder(self)-> Callable[[IMessageBuilderContext[T,TProperty]],str]: return None
-
-    @property
-    def CascadeMode(self)-> Callable[[],CascadeMode]: return self._cascadeModeThunk()
-    @CascadeMode.setter
-    def CascadeMode(self,value): lambda: value
-
-
-    @staticmethod
-    def PrepareMessageFormatterForValidationError(context:ValidationContext[T], value:TValue)->None:
-        # context.MessageFormatter.AppendPropertyName(context.DisplayName)
-        context.MessageFormatter.AppendPropertyValue(value)
-        # context.MessageFormatter.AppendArgument("PropertyPath", context.PropertyPath)
-
-
-
-    def CreateValidationError(self, context:ValidationContext[T], value:TValue, component:RuleComponent[T, TValue])->ValidationFailure: 
-        if self.MessageBuilder is not None:
-            error =self.MessageBuilder(MessageBuilderContext[T,TProperty](context,value,component))
-        else:
-            error = component.GetErrorMessage(context,value)
-
-        failure = ValidationFailure(context.PropertyPath, error, value,component.ErrorCode)
-
-        # failure.FormattedMessagePlaceholderValues = context.MessageFormatter.PlaceholderValues
-        failure._ErrorCode = component.ErrorCode # ?? ValidatorOptions.Global.ErrorCodeResolver(component.Validator);
-
-        return failure
-
-
-
-class PropertyRule[T,TProperty](RuleBase[T,TProperty,TProperty]):
-    def __init__(self
-                 , func:Callable[[T],TProperty]
-                 , cascadeModeThunk:Callable[[],CascadeMode]
-                 , type_to_validate:type
-                )->None:
-        super().__init__(func, cascadeModeThunk, type_to_validate)
-
-    def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} from '{self.PropertyName}' at {hex(id(self))}>"
-
-    @classmethod
-    def create(cls, func:Callable[[T],TProperty], cascadeModeThunk:Callable[[],CascadeMode] )->Self:
-        return PropertyRule(func, cascadeModeThunk, type(TProperty))
-
-    def AddValidator(self,validator:IPropertyValidator[T,TProperty])->None:
-        component:RuleComponent = RuleComponent[T,TProperty](validator)
-        self._components.append(component)
-        return None
-    
-    def GetDisplayName(): ...
-
-
-    def ValidateAsync(self, context:ValidationContext[T])-> None:
-        first = True
-        cascade = self.CascadeMode
-        total_failures = len(context.Failures)
-        context.InitializeForPropertyValidator(self.PropertyName)
-        for component in self.Components:
-            context.MessageFormatter.Reset()
-            if first:
-                first = False
-                propValue= self.PropertyFunc(context.instance_to_validate)
-
-            valid:bool = component.ValidateAsync(context,propValue)
-            if not valid:
-                # super().PrepareMessageFormatterForValidationError(context,propValue)
-                failure = self.CreateValidationError(context,propValue,component)
-                context.Failures.append(failure)
-            if len(context.Failures)> total_failures and cascade == CascadeMode.Stop:
-                break
-
-        return None
-    
-
-class RuleBuilder[T,TProperty](IRuleBuilder,IRuleBuilderInternal): # no implemento IRuleBuilderOptions por que el metodo no se que hace
-
-    def __init__(self,rule:IValidationRuleInternal[T,TProperty], parent:TAbstractValidator):
-        self._rule = rule
-        self.parent_validator = parent
-
-    def SetValidator(self,validator:IPropertyValidator[T,TProperty])->IRuleBuilder[T,TProperty]: # -> IRuleBuilderOptions[T,TProperty]
-        self.Rule.AddValidator(validator)
-        return self
-    
-    @property
-    def Rule(self) -> IValidationRule[T, TProperty]:
-        return self._rule
-        
-
 
 class AbstractValidator[T](ABC):
     #region constructor
@@ -157,6 +23,7 @@ class AbstractValidator[T](ABC):
         result:ValidationResult = ValidationResult(errors=context.Failures)
         for rule in self._rules:
             rule.ValidateAsync(context)
+            #FIXME [ ] al modificar ClassLevelCascade fuera de esta clase, el enum no es el mismo pues no tiene la misma direccion en memoria
             if self.ClassLevelCascadeMode== CascadeMode.Stop and len(result.errors)> 0:
                 break
  

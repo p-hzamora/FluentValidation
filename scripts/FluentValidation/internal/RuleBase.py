afbdc11a9d8704b3da35e60e7fa164b4e1cbcb61
phzamora <p.hzamora@icloud.com>
2023-12-13 13:25:14 +0100

    refactor: detach the "RuleBase" class from "AbstractValidator"

---
+++ scripts/FluentValidation/internal/RuleBase.py
@@ -0,0 +1,71 @@
+from typing import Callable, List
+import dis
+
+from IValidationRule import IValidationRule
+from internal.MessageBuilderContext import MessageBuilderContext
+from internal.RuleComponent import RuleComponent
+from results.ValidationFailure import ValidationFailure
+
+
+from IValidationContext import ValidationContext
+from IValidationRule import *
+from syntax import *
+from DefaultValidatorExtensions import CascadeMode
+
+
+class RuleBase[T,TProperty,TValue](IValidationRule[T,TValue]):
+    def __init__(self,propertyFunc:Callable[[T],TProperty], cascadeModeThunk:Callable[[],CascadeMode], type_to_validate:type):
+        self._PropertyFunc = propertyFunc
+        self._type_to_validate = type_to_validate
+        self._cascadeModeThunk:Callable[[],CascadeMode] = cascadeModeThunk
+        self._components:List[RuleComponent[T, TProperty]] = []
+        self._propertyName:str = {x.opname:x.argval for x in dis.Bytecode(propertyFunc)}["LOAD_ATTR"]
+        self._displayName:str = self._propertyName
+
+    @property
+    def PropertyFunc(self)->Callable[[T],TProperty]: return self._PropertyFunc
+    @property
+    def TypeToValidate(self): return self._type_to_validate
+
+    @property
+    def Components(self): return self._components
+
+    @property
+    def PropertyName(self): return self._propertyName
+    
+    @property
+    def displayName(self): return self._displayName
+
+    @property
+    def Current(self)->IRuleComponent: return self._components[-1]
+
+    @property
+    def MessageBuilder(self)-> Callable[[IMessageBuilderContext[T,TProperty]],str]: return None
+
+    @property
+    def CascadeMode(self)-> Callable[[],CascadeMode]: return self._cascadeModeThunk()
+    @CascadeMode.setter
+    def CascadeMode(self,value): lambda: value
+
+
+    @staticmethod
+    def PrepareMessageFormatterForValidationError(context:ValidationContext[T], value:TValue)->None:
+        # context.MessageFormatter.AppendPropertyName(context.DisplayName)
+        context.MessageFormatter.AppendPropertyValue(value)
+        # context.MessageFormatter.AppendArgument("PropertyPath", context.PropertyPath)
+
+
+
+    def CreateValidationError(self, context:ValidationContext[T], value:TValue, component:RuleComponent[T, TValue])->ValidationFailure: 
+        if self.MessageBuilder is not None:
+            error =self.MessageBuilder(MessageBuilderContext[T,TProperty](context,value,component))
+        else:
+            error = component.GetErrorMessage(context,value)
+
+        failure = ValidationFailure(context.PropertyPath, error, value,component.ErrorCode)
+
+        # failure.FormattedMessagePlaceholderValues = context.MessageFormatter.PlaceholderValues
+        failure._ErrorCode = component.ErrorCode # ?? ValidatorOptions.Global.ErrorCodeResolver(component.Validator);
+
+        return failure
+

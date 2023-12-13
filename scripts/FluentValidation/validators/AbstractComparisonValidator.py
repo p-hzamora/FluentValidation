1fdc92e87610743d364e2416f7a90d3c9be30c98
phzamora <p.hzamora@icloud.com>
2023-12-13 13:34:44 +0100

    feat: created Comparable class

--- scripts/FluentValidation/validators/AbstractComparisonValidator.py
+++ scripts/FluentValidation/validators/AbstractComparisonValidator.py
@@ -6,13 +6,31 @@ from stc.common.scripts.FluentValidation.IValidationContext import ValidationCon
 from stc.common.scripts.FluentValidation.validators.PropertyValidator import PropertyValidator
 from stc.common.scripts.FluentValidation.validators.IpropertyValidator import IPropertyValidator
 
-class IComparable[T](ABC):
-        @abstractmethod
-        def CompareTo(other:T)->int: ...
+IComparable = TypeVar("IComparable",bound="Comparable")
 
-class MemberInfo():
-	...
+class Comparable(object):
+    def __init__(self, value):
+        self._value = value
 
+    def __lt__(self, __value: IComparable) -> bool:
+        return self._value < __value._value
+
+    def __le__(self, __value: IComparable) -> bool:
+        return self._value <= __value._value
+
+    def __eq__(self, __value: IComparable) -> bool:
+        return self._value == __value._value
+
+    def __nq__(self, __value: IComparable) -> bool:
+        return self._value != __value._value
+
+    def __gt__(self, __value: IComparable) -> bool:
+        return self._value > __value._value
+
+    def __ge__(self, __value: IComparable) -> bool:
+        return self._value >= __value._value
+
+    
 
 class Comparison(Enum):
     Equal = 1

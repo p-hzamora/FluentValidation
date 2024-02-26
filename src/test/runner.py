865a730678db5d66ed8e08016db182ce154f7541
phzamora <p.hzamora@icloud.com>
2024-02-26 14:26:31 +0100

    refactor: runner.py

--- src/test/runner.py
+++ src/test/runner.py
@@ -14,14 +14,18 @@ loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(test_AbstractValidator))
-suite.addTests(loader.loadTestsFromModule(test_Equal))
-suite.addTests(loader.loadTestsFromModule(test_GreaterThanOrEqual))
-suite.addTests(loader.loadTestsFromModule(test_GreaterThanValidator))
-suite.addTests(loader.loadTestsFromModule(test_LessThanOrEqual))
-suite.addTests(loader.loadTestsFromModule(test_LessThanValidator))
-suite.addTest(loader.loadTestsFromModule(test_LengtValidator))
-suite.addTest(loader.loadTestsFromModule(test_PredicateValidator))
+suite.addTests(
+    [
+        *loader.loadTestsFromModule(test_AbstractValidator),
+        *loader.loadTestsFromModule(test_Equal),
+        *loader.loadTestsFromModule(test_GreaterThanOrEqual),
+        *loader.loadTestsFromModule(test_GreaterThanValidator),
+        *loader.loadTestsFromModule(test_LessThanOrEqual),
+        *loader.loadTestsFromModule(test_LessThanValidator),
+        *loader.loadTestsFromModule(test_LengtValidator),
+        *loader.loadTestsFromModule(test_PredicateValidator),
+    ]
+)
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner()

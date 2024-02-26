43fefdf7abedecda6d89f6b9ecb5ddac47a0290e
phzamora <p.hzamora@icloud.com>
2024-02-26 11:39:32 +0100

    feat: added CultureScope to set lenguage to testing purpose

---
+++ src/test/CultureScope.py
@@ -0,0 +1,9 @@
+
+from src.FluentValidation.internal.Resources.ILanguageManager import CultureInfo
+
+class CultureScope:
+    CurrentCulture:CultureInfo = None
+
+    @classmethod
+    def SetDefaultCulture(cls):
+        cls.CurrentCulture = CultureInfo("en-US")

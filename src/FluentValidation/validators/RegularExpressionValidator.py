03d36afc4d495ffa754778de13df66efb6b92645
phzamora <p.hzamora@icloud.com>
2024-02-26 09:13:00 +0100

    style: applied Ruff

--- src/FluentValidation/validators/RegularExpressionValidator.py
+++ src/FluentValidation/validators/RegularExpressionValidator.py
@@ -1,51 +1,60 @@
-
 from dataclasses import dataclass
 
 import re
-from typing import override
+from typing import overload, override
 from ..IValidationContext import ValidationContext
 
 from ..validators.PropertyValidator import PropertyValidator
 
 
-class IRegularExpressionValidator():
-    _expression:str
-    _regex_func:re.Pattern
+class IRegularExpressionValidator:
+    _expression: str
+    _regex_func: re.Pattern
+
 
 @dataclass
-class RegularExpressionValidator[T](PropertyValidator[T,str],IRegularExpressionValidator): 
+class RegularExpressionValidator[T](PropertyValidator[T, str], IRegularExpressionValidator):
+    @overload
+    def __init__(self, expression: str):
+        ...
 
-    def __init__(self,expression:str|re.Pattern, options:re.RegexFlag=None):
-        if isinstance(expression,str):
+    @overload
+    def __init__(self, expression: re.Pattern):
+        ...
+
+    @overload
+    def __init__(self, expression: str, options: re.RegexFlag):
+        ...
+
+    def __init__(self, expression: str | re.Pattern, options: re.RegexFlag = None):
+        if isinstance(expression, str) and options is None:
             self.__init__exp_str(expression)
-        elif isinstance(expression,re.Pattern) and options is not None:
+        elif isinstance(expression, re.Pattern) and options is None:
             self.__init__exp_re_pattern(expression)
-        elif isinstance(expression,re.Pattern) and options is None:
-            self.__init__exp_re_pattern_options(expression,options)
+        elif isinstance(expression, re.Pattern) and options is not None:
+            self.__init__exp_re_pattern_options(expression, options)
         else:
             raise Exception("No se ha inicializado la variable correctamente")
 
-
-    def __init__exp_str(self,expression:str):
+    def __init__exp_str(self, expression: str):
         self._expression = expression
         self._regex_func = re.compile(expression)
 
-    def __init__exp_re_pattern(self,expression:re.Pattern):
+    def __init__exp_re_pattern(self, expression: re.Pattern):
         self._regex_func = expression
 
-    def __init__exp_re_pattern_options(self,expression:str,options:re.RegexFlag):
+    def __init__exp_re_pattern_options(self, expression: str, options: re.RegexFlag):
         self._expression = expression
-        self._regex_func = re.compile(expression,options)
+        self._regex_func = re.compile(expression, options)
 
     @override
-    def is_valid(self,context: ValidationContext[T], value: str):
+    def is_valid(self, context: ValidationContext[T], value: str):
         if value is None:
             return True
-        
+
         # regex:re.Pattern= self._regex_func(context.instance_to_validate)
 
         if not self._regex_func.match(value):
-            context.MessageFormatter.AppendArgument(self.Name,str(self._regex_func))
+            context.MessageFormatter.AppendArgument(self.Name, str(self._regex_func))
             return False
-        return True 
-    
+        return True

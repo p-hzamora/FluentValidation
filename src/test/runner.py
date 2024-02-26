61fd4817cefe6d7a4f3b9ec49c5076a5cc431b00
phzamora <p.hzamora@icloud.com>
2024-02-26 09:59:36 +0100

    test: added test_PredicateValidator.py

--- src/test/runner.py
+++ src/test/runner.py
@@ -6,6 +6,7 @@ import test_GreaterThanValidator
 import test_LessThanOrEqual
 import test_LessThanValidator
 import test_LengtValidator
+import test_PredicateValidator
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -18,6 +19,7 @@ suite.addTests(loader.loadTestsFromModule(test_GreaterThanValidator))
 suite.addTests(loader.loadTestsFromModule(test_LessThanOrEqual))
 suite.addTests(loader.loadTestsFromModule(test_LessThanValidator))
 suite.addTest(loader.loadTestsFromModule(test_LengtValidator))
+suite.addTest(loader.loadTestsFromModule(test_PredicateValidator))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner()

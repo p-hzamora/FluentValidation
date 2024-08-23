f8f6a9db77cd6d669c7eef52452cfd1d7a82c8df
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:39:13 +0200

    update!: added 'options' attr in 'validate' method to only validate specific properties!!

--- src/fluent_validation/abstract_validator.py
+++ src/fluent_validation/abstract_validator.py
@@ -1,21 +1,23 @@
 from __future__ import annotations
-from typing import Callable, Optional, override, TYPE_CHECKING
+from typing import Callable, Optional, overload, override, TYPE_CHECKING
 import asyncio
 
 if TYPE_CHECKING:
     from src.fluent_validation.internal.ValidationStrategy import ValidationStrategy
 
+from ..fluent_validation.IValidator import IValidator  # noqa: F401 We use it in the future
 from .results.ValidationResult import ValidationResult
-from .IValidationContext import ValidationContext
+from .IValidationContext import IValidationContext, ValidationContext
 from .syntax import IRuleBuilder
 from .internal.PropertyRule import PropertyRule
 from .internal.RuleBuilder import RuleBuilder
+from .internal.RuleSetValidatorSelector import RulesetValidatorSelector
 
 from .ValidatorOptions import ValidatorOptions
 from .enums import CascadeMode
 
 
-class AbstractValidator[T](ABC):
+class AbstractValidator[T](IValidator[T]):
     # region constructor
     def __init__(self) -> None:
         self._classLevelCascadeMode: Callable[[], CascadeMode] = lambda: ValidatorOptions.Global.DefaultClassLevelCascadeMode
@@ -24,33 +26,69 @@ class AbstractValidator[T](ABC):
 
     # endregion
 
-    # region Private methods
-    def internal_validate(self, context: ValidationContext) -> ValidationResult:
+
+    @overload
+    def validate(self, instance: T) -> ValidationResult: ...
+
+    @overload
+    def validate(self, instance: IValidationContext) -> ValidationResult: ...
+
+    @overload
+    def validate(self, instance: T, options: Callable[[ValidationStrategy[T]], None]) -> ValidationResult: ...
+
+    @override
+    def validate(self, instance: T | IValidationContext, options: Optional[Callable[[ValidationStrategy[T]], None]] = None) -> ValidationResult:
+        if options:
+            return self.validate(ValidationContext[T].CreateWithOptions(instance, options))
+
+        if not options and isinstance(instance,IValidationContext):
+            # instance acts as context, due to does not exists override operator as C#, I need to call context attr as instance
+            return self.__validate__(ValidationContext[T].GetFromNonGenericContext(instance))
+
+        return self.__validate__(ValidationContext(instance, None, ValidatorOptions.Global.ValidatorSelectors.DefaultValidatorSelectorFactory()))
+
+    def __validate__(self, context: ValidationContext[T]) -> ValidationResult:
+        try:
+            return asyncio.run(self.ValidateInternalAsync(context, False))
+        except Exception:
+            raise Exception
+
+    async def ValidateInternalAsync(self, context: ValidationContext[T], useAsync: bool) -> ValidationResult:
         result: ValidationResult = ValidationResult(errors=context.Failures)
-        for rule in self._rules:
-            rule.ValidateAsync(context)
+
+        count: int = len(self._rules)
+        for i in range(count):
+            await self._rules[i].ValidateAsync(context, useAsync)
+
             if self.ClassLevelCascadeMode == CascadeMode.Stop and len(result.errors) > 0:
                 break
 
-        # self.SetExecutedRuleSets(result,context)
+        self.SetExecutedRuleSets(result, context)
+
+        # if (!result.IsValid && context.ThrowOnFailures) {
+        #     RaiseValidationException(context, result);
+        # }
         return result
 
+        # COMMENT: used in private async ValueTask<ValidationResult> ValidateInternalAsync(ValidationContext<T> context, bool useAsync, CancellationToken cancellation) {...}
+
     def SetExecutedRuleSets(self, result: ValidationResult, context: ValidationContext[T]):
-        ...
-        # result.RuleSetExecuted = RulesetValidatorSelector.DefaultRuleSetNameInArray
-
-    # endregion
-
-    # region Public methods
-    def validate(self, instance: T) -> ValidationResult:
-        return self.internal_validate(ValidationContext(instance))
+        obj = context.RootContextData.get("_FV_RuleSetsExecuted", None)
+        if obj and isinstance(obj, set):
+            result.RuleSetsExecuted = list(obj)
+        else:
+            result.RuleSetsExecuted = RulesetValidatorSelector.DefaultRuleSetNameInArray
 
     def rule_for[TProperty](self, func: Callable[[T], TProperty]) -> IRuleBuilder[T, TProperty]:  # IRuleBuilderInitial[T,TProperty]:
         rule: PropertyRule[T, TProperty] = PropertyRule.create(func, lambda: self.RuleLevelCascadeMode)
         self._rules.append(rule)
         return RuleBuilder[T, TProperty](rule, self)
 
-    # endregion
+    # FIXME [ ]: It's wrong implementation
+    def rule_set(self, rule_set_name: str, action: Callable[..., None]) -> None:
+        rule_set_names = [name.strip() for name in rule_set_name.split(",;")]
+        setattr(self._rules, lambda r: setattr(r, "RuleSets", rule_set_names))
+        return None
 
     # region Properties
     @property

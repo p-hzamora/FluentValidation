1e759e981d5059dd94b469afa29ecc5db2625711
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:11:46 +0200

    fix: implemented interfaces

--- src/test/person.py
+++ src/test/person.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import datetime
 from decimal import Decimal
 from enum import Enum
+from typing import override
 
 
 class Country:
@@ -20,56 +21,107 @@ class EnumGender(Enum):
 class IAddress(ABC):
     @property
     @abstractmethod
-    def Line1(self) -> str:
-        ...
+    def Line1(self) -> str: ...
 
     @property
     @abstractmethod
-    def Line2(self) -> str:
-        ...
+    def Line2(self) -> str: ...
 
     @property
     @abstractmethod
-    def Town(self) -> str:
-        ...
+    def Town(self) -> str: ...
 
     @property
     @abstractmethod
-    def County(self) -> str:
-        ...
+    def County(self) -> str: ...
 
     @property
     @abstractmethod
-    def Postcode(self) -> str:
-        ...
+    def Postcode(self) -> str: ...
 
     @property
     @abstractmethod
-    def Country(self) -> Country:
-        ...
+    def Country(self) -> Country: ...
 
 
 class Address(IAddress):
-    Line1: str
-    Line2: str
-    Town: str
-    County: str
-    Postcode: str
-    Country: Country
-    Id: int
+    def __init__(
+        self,
+        Line1: str,
+        Line2: str,
+        Town: str,
+        County: str,
+        Postcode: str,
+        Country: Country,
+        Id: int,
+    ):
+        self._Line1: str = Line1
+        self._Line2: str = Line2
+        self._Town: str = Town
+        self._County: str = County
+        self._Postcode: str = Postcode
+        self._Country: Country = Country
+        self._Id: int = Id
+
+    @override
+    @property
+    def Line1(self) -> str:
+        return self._Line1
+
+    @override
+    @property
+    def Line2(self) -> str:
+        return self._Line2
+
+    @override
+    @property
+    def Town(self) -> str:
+        return self._Town
+
+    @override
+    @property
+    def County(self) -> str:
+        return self._County
+
+    @override
+    @property
+    def Postcode(self) -> str:
+        return self._Postcode
+
+    @override
+    @property
+    def Country(self) -> Country:
+        return self._Country
+
+    @override
+    @property
+    def Id(self) -> int:
+        return self._Id
 
 
 class IOrder(ABC):
     @property
     @abstractmethod
-    def Amount(self) -> Decimal:
-        ...
+    def Amount(self) -> Decimal: ...
 
 
 class Order(IOrder):
-    ProductName: str
-    Amount: Decimal
-    Payments: list["Payment"]
+    def __init__(self, Amount: Decimal, ProductName: str, Payments: list["Payment"]):
+        self._Amount: Decimal = Amount
+        self._ProductName: str = ProductName
+        self._Payments: list["Payment"] = Payments
+
+    @property
+    def ProductName(self) -> str:
+        return self._ProductName
+
+    @property
+    def Amount(self) -> Decimal:
+        return self._Amount
+
+    @property
+    def Payments(self) -> list["Payment"]:
+        return self._Payments
 
 
 class Person:

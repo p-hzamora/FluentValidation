ec8e3a251f1592fb0b01968647ab15b5bc3d0ee9
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:31:12 +0200

    update: 'MemberNameValidatorSelector' class

--- src/fluent_validation/internal/MemberNameValidatorSelector.py
+++ src/fluent_validation/internal/MemberNameValidatorSelector.py
@@ -12,37 +12,36 @@ if TYPE_CHECKING:
 
 
 class MemberNameValidatorSelector(IValidatorSelector):
-    DisableCascadeKey:str = "_FV_DisableSelectorCascadeForChildRules"
-    
-    _collectionIndexNormalizer:re.Pattern[str] = re.compile(r"\[.*?\]")
+    DisableCascadeKey: str = "_FV_DisableSelectorCascadeForChildRules"
 
-    def __init__(self, memberNames:Iterable[str]):
-        self._memberNames:Iterable[str] = memberNames
+    _collectionIndexNormalizer: re.Pattern[str] = re.compile(r"\[.*?\]")
+
+    def __init__(self, memberNames: Iterable[str]):
+        self._memberNames: Iterable[str] = memberNames
 
     @property
-    def MemberNames(self)->Iterable[str]:
+    def MemberNames(self) -> Iterable[str]:
         return self._memberNames
-    
 
     @override
-    def CanExecute (self, rule: IValidationRule, propertyPath:str, context: IValidationContext)->bool:
+    def CanExecute(self, rule: IValidationRule, propertyPath: str, context: IValidationContext) -> bool:
         # Validator selector only applies to the top level.
         # If we're running in a child context then this means that the child validator has already been selected
         # Because of this, we assume that the rule should continue (ie if the parent rule is valid, all children are valid)
-        isChildContext:bool = context.IsChildContext
-        cascadeEnabled:bool = self.DisableCascadeKey not in context.RootContextData
+        isChildContext: bool = context.IsChildContext
+        cascadeEnabled: bool = self.DisableCascadeKey not in context.RootContextData
 
         # If a child validator is being executed and the cascade is enabled (which is the default)
-		# then the child validator's rule should always be included.
-		# The only time this isn't the case is if the member names contained for inclusion are for child
-		# properties (which is indicated by them containing a period).
-        if isChildContext & cascadeEnabled and not any(["." in x for x in self._memberNames]):
+        # then the child validator's rule should always be included.
+        # The only time this isn't the case is if the member names contained for inclusion are for child
+        # properties (which is indicated by them containing a period).
+        if isChildContext and cascadeEnabled and not any(["." in x for x in self._memberNames]):
             return True
 
-        if issubclass(rule,IIncludeRule):
+        if isinstance(rule, IIncludeRule):
             return True
 
-        normalizedPropertyPath:Optional[str] = None
+        normalizedPropertyPath: Optional[str] = None
 
         for memberName in self._memberNames:
             if memberName == propertyPath:
@@ -71,19 +70,22 @@ class MemberNameValidatorSelector(IValidatorSelector):
                     return True
 
         return False
-    
-    #TODOL: Check if it correct 	public static string[] MemberNamesFromExpressions<T>(params Expression<Func<T, object>>[] propertyExpressions) {
 
-    def MemberNamesFromExpressions[T](self, *propertyExpressions:  Callable[[T],Any])->list[str]:
-        members:list[str] = [propertyExpressions.Select(self.MemberFromExpression)]
+    # TODOL: Check if it correct 	public static string[] MemberNamesFromExpressions<T>(params Expression<Func<T, object>>[] propertyExpressions) {
+
+    @classmethod
+    def MemberNamesFromExpressions[T](cls, *propertyExpressions: Callable[[T], Any]) -> list[str]:
+        members: list[str] = [cls.MemberFromExpression(x) for x in propertyExpressions]
         return members
 
     @staticmethod
-    def MemberFromExpression[T](expression:Callable[[T],Any])->str:
-        #TODOH: Implements Disassembler class developed for ormlambda
-        propertyName:str = "___"#ValidatorOptions.Global.PropertyNameResolver(, expression.GetMember(), expression)
+    def MemberFromExpression[T](expression: Callable[[T], Any]) -> str:
+        from src.fluent_validation.ValidatorOptions import ValidatorOptions
 
-        if propertyName == "" or propertyName.isspace() or propertyName is None:
+        # get list of all values in expression (one is expected) and get first
+        propertyName = ValidatorOptions.Global.PropertyNameResolver(expression).to_list()
+
+        if not propertyName:
             raise ValueError(f"Expression '{expression}' does not specify a valid property or field.")
-        
-        return propertyName
+
+        return propertyName[0].nested_element.name

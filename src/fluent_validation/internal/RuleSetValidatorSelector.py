3fdaa468de56bf967a9fa36033916bdd55d78d70
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:34:02 +0200

    update: 'CanExecute' method from 'RulesetValidatorSelector' class

--- src/fluent_validation/internal/RuleSetValidatorSelector.py
+++ src/fluent_validation/internal/RuleSetValidatorSelector.py
@@ -8,35 +8,34 @@ if TYPE_CHECKING:
     from src.fluent_validation.IValidationContext import IValidationContext
 
 
-def get_or_add(dictionary:dict, key:str, factory)->set:
+def get_or_add(dictionary: dict, key: str, factory) -> set:
     if key not in dictionary:
         dictionary[key] = factory()
     return dictionary[key]
 
-class RulesetValidatorSelector(IValidatorSelector):
-    
-    DefaultRuleSetName:str = "default"
-    WildcardRuleSetName:str = "*"
 
-    DefaultRuleSetNameInArray:list[str]= [DefaultRuleSetName]
+class RulesetValidatorSelector(IValidatorSelector):
+    DefaultRuleSetName: str = "default"
+    WildcardRuleSetName: str = "*"
+
+    DefaultRuleSetNameInArray: list[str] = [DefaultRuleSetName]
 
     @property
-    def RuleSets(self)->Iterable[str]:
+    def RuleSets(self) -> Iterable[str]:
         return self._rulesetsToExecute
 
-    def __init__(self, rulesetsToExecute:Iterable[str]):
-        self._rulesetsToExecute:Iterable[str] = rulesetsToExecute
+    def __init__(self, rulesetsToExecute: Iterable[str]):
+        self._rulesetsToExecute: Iterable[str] = rulesetsToExecute
 
     @override
-    def CanExecute(self, rule:IValidationRule, propertyPath:str, context:IValidationContext):
+    def CanExecute(self, rule: IValidationRule, propertyPath: str, context: IValidationContext):
+        executed: set = get_or_add(context.RootContextData, "_FV_RuleSetsExecuted", lambda: set())
 
-        executed:set = get_or_add(context.RootContextData,"_FV_RuleSetsExecuted", lambda: set())
-
-        if (rule.RuleSets is None | len(rule.RuleSets) == 0) & self._rulesetsToExecute.Any():
+        if (rule.RuleSets is None | len(rule.RuleSets) == 0) and self._rulesetsToExecute:
             if self.IsIncludeRule(rule):
                 return True
 
-        if (rule.RuleSets is None | len(rule.RuleSets) == 0) and not self._rulesetsToExecute.Any():
+        if (rule.RuleSets is None | len(rule.RuleSets) == 0) and not self._rulesetsToExecute:
             executed.add(self.DefaultRuleSetName)
             return True
 
@@ -45,12 +44,12 @@ class RulesetValidatorSelector(IValidatorSelector):
                 executed.add(self.DefaultRuleSetName)
                 return True
 
-        # if rule.RuleSets is not None & len(rule.RuleSets) > 0 & self._rulesetsToExecute.Any():
-        #     #FIXME [ ]: 
-        #     intersection = [rule.RuleSets.intersection(self._rulesetsToExecute.lower())]
-        #     if intersection.Any():
-        #         for r in intersection.ForEach(r => executed.add(r))
-        #         return True
+        if rule.RuleSets is not None & len(rule.RuleSets) > 0 & self._rulesetsToExecute:
+            intersection = [rule.RuleSets - set([x.lower() for x in self._rulesetsToExecute])]
+            if intersection:
+                for r in intersection:
+                    executed.add(r)
+                return True
 
         if self.WildcardRuleSetName in self._rulesetsToExecute:
             if rule.RuleSets is None | len(rule.RuleSets) == 0:
@@ -61,15 +60,14 @@ class RulesetValidatorSelector(IValidatorSelector):
             return True
         return False
 
-    def IsIncludeRule(rule:IValidationRule)->bool:
-        return issubclass(rule,IIncludeRule)
+    def IsIncludeRule(rule: IValidationRule) -> bool:
+        return isinstance(rule, IIncludeRule)
 
     @staticmethod
-    def LegacyRulesetSplit(ruleSet:str)->list[str]:
-        return [x.strip() for x in ruleSet.split(",","")]
+    def LegacyRulesetSplit(ruleSet: str) -> list[str]:
+        return [x.strip() for x in ruleSet.split(",", "")]
 
-
-    #TODOH: Check
+    # TODOH: Check
 
     # internal static string[] LegacyRulesetSplit(string ruleSet) {
     # 	var ruleSetNames = ruleSet.Split(',', ';')

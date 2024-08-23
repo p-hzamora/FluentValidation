78926046be27acf98f671ad49237836ff33ce29f
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:35:20 +0200

    update: added methods in 'RuleComponent'

--- src/fluent_validation/internal/RuleComponent.py
+++ src/fluent_validation/internal/RuleComponent.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional
 from ..IValidationContext import ValidationContext
 from ..internal.IRuleComponent import IRuleComponent
 from ..validators.IpropertyValidator import IPropertyValidator
@@ -7,8 +7,11 @@ from ..validators.IpropertyValidator import IPropertyValidator
 class RuleComponent[T, TProperty](IRuleComponent):
     def __init__(self, property_validator: IPropertyValidator[T, TProperty]) -> None:
         self._property_validator: IPropertyValidator[T, TProperty] = property_validator
-        self._error_message = property_validator.get_default_message_template(self.ErrorCode)
+        self._error_message:Optional[str] = None
+        # self._asyncPropertyValidator:IAsyncPropertyValidator[T,TProperty]= None
         self._errorMessageFactory: Callable[[ValidationContext], T] = None
+
+        self._condition: Callable[[ValidationContext[T], bool]] = None
 
     def __repr__(self) -> str:
         return f"<RuleComponent validator: {self.ErrorCode}>"
@@ -24,15 +27,20 @@ class RuleComponent[T, TProperty](IRuleComponent):
     def set_error_message(self, error_message: str) -> None:
         self._error_message = error_message
 
-    def invoke_property_validator(self, context: ValidationContext[T], value: TProperty) -> bool:
+    async def ValidateAsync(self, context: ValidationContext[T], value: TProperty, useAsync: bool) -> bool:
+        if useAsync:
+            return await self.InvokePropertyValidatorAsync(context, value)
+        return self.InvokePropertyValidator(context, value)
+
+    async def InvokePropertyValidatorAsync(self, context: ValidationContext[T], value: TProperty):
+        return self._asyncPropertyValidator.IsValidAsync(context, value)
+
+    def InvokePropertyValidator(self, context: ValidationContext[T], value: TProperty) -> bool:
         return self.Validator.is_valid(context, value)
 
-    def ValidateAsync(self, context: ValidationContext[T], value: TProperty) -> bool:
-        return self.invoke_property_validator(context, value)
-
-    def GetErrorMessage(self, context: ValidationContext[T], value: TProperty):
-        # FIXME [ ]: self._error_message has value when it must by empty test "test_When_the_maxlength_validator_fails_the_error_message_should_be_set"
-        rawTemplate: str = setattr(self._errorMessageFactory, value) if self._errorMessageFactory else self._error_message
+    def GetErrorMessage(self, context: Optional[ValidationContext[T]], value: TProperty):
+        # FIXME [x]: self._error_message has value when it must by empty test "test_When_the_maxlength_validator_fails_the_error_message_should_be_set"
+        rawTemplate:Optional[str] = setattr(self._errorMessageFactory, value) if self._errorMessageFactory else self._error_message
         if rawTemplate is None:
             rawTemplate = self.Validator.get_default_message_template(self.ErrorCode)  # original
 
@@ -40,3 +48,8 @@ class RuleComponent[T, TProperty](IRuleComponent):
             return rawTemplate
 
         return context.MessageFormatter.BuildMessage(rawTemplate)
+
+    def InvokeCondition(self, context: ValidationContext[T]) -> bool:
+        if self._condition is not None:
+            return self._condition(context)
+        return True

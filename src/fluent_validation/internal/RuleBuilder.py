b8a585a81a2ceb7472ef11a7d87259e68fbd752b
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:21:55 +0200

    refactor: 'set_validator' method from 'RuleBuilder' class

--- src/fluent_validation/internal/RuleBuilder.py
+++ src/fluent_validation/internal/RuleBuilder.py
@@ -1,8 +1,11 @@
-from typing import TypeVar
+import inspect
+from typing import Callable, TypeVar
+
+from src.fluent_validation.IValidator import IValidator
 
 from ..IValidationRule import IValidationRule, IValidationRuleInternal
 from ..validators.IpropertyValidator import IPropertyValidator
-from ..syntax import IRuleBuilder, IRuleBuilderInternal
+from ..syntax import IRuleBuilder, IRuleBuilderInternal, IRuleBuilderOptions
 
 
 TAbstractValidator = TypeVar("TAbstractValidator")
@@ -13,10 +16,42 @@ class RuleBuilder[T, TProperty](IRuleBuilder[T, TProperty], IRuleBuilderInternal
         self._rule = rule
         self.parent_validator = parent
 
-    def set_validator(self, validator: IPropertyValidator[T, TProperty]) -> IRuleBuilder[T, TProperty]:  # -> IRuleBuilderOptions[T,TProperty]
-        self.Rule.AddValidator(validator)
-        return self
-
     @property
     def Rule(self) -> IValidationRule[T, TProperty]:
         return self._rule
+
+    def set_validator(self, validator, *ruleSets) -> IRuleBuilderOptions[T, TProperty]:
+        if isinstance(validator, IPropertyValidator):
+            return self.set_validator_IPropertyValidator(validator)
+
+        elif isinstance(validator, IValidator):
+            return self.set_validator_IValidator(validator, ruleSets)
+
+        elif callable(validator) and len(inspect.signature(validator).parameters) == 1:
+            return self.set_validator_Callable_T(validator, ruleSets)
+
+        elif callable(validator) and len(inspect.signature(validator).parameters) == 2:
+            return self.set_validator_Callable_T_TProperty(validator, ruleSets)
+
+        else:
+            raise AttributeError(validator)
+
+    def set_validator_IPropertyValidator(self, validator: IPropertyValidator[T, TProperty]) -> IRuleBuilderOptions[T, TProperty]:
+        self.Rule.AddValidator(validator)
+        return self
+
+    def set_validator_IValidator(self, validator: IValidator[TProperty], *ruleSets: str) -> IRuleBuilderOptions[T, TProperty]:
+        # TODOH []: Create ChildValidatorAdaptor class ASAP
+        ...
+        # adaptor = ChildValidatorAdaptor[T,TProperty](validator,type(validator))
+        # adaptor.RuleSets = ruleSets
+
+        # self.Rule.AddAsyncValidator(adaptor,adaptor)
+
+    def set_validator_Callable_T[TValidator: IValidator[TProperty]](self, validator: Callable[[T], TValidator], *ruleSets: str) -> IRuleBuilderOptions[T, TProperty]: 
+        #TODOH []: We need to implement this method to use set_validator properly
+        ...
+
+    def set_validator_Callable_T_TProperty[TValidator: IValidator[TProperty]](self, validator: Callable[[T, TProperty], TValidator], *ruleSets: str) -> IRuleBuilderOptions[T, TProperty]: 
+        #TODOH []: We need to implement this method to use set_validator properly
+        ...

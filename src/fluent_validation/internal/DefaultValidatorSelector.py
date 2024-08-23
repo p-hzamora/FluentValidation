9d47e0835d1384820be7f6ce262840fc991421ea
p-hzamora <p.hzamora@icloud.com>
2024-08-23 15:30:48 +0200

    update: 'CanExecute' method from the 'DefaultValidatorSelector' class

--- src/fluent_validation/internal/DefaultValidatorSelector.py
+++ src/fluent_validation/internal/DefaultValidatorSelector.py
@@ -10,9 +10,10 @@ if TYPE_CHECKING:
 
 class DefaultValidatorSelector(IValidatorSelector):
     @override
+    @staticmethod
     def CanExecute(rule:IValidationRule, propertyPath:str,context:IValidationContext):
         #By default we ignore any rules part of a RuleSet.
-        if rule.RuleSets is not None & len(rule.RuleSets) > 0 and RulesetValidatorSelector.DefaultRuleSetName.lower() not in rule.RuleSets:
+        if rule.RuleSets is not None and len(rule.RuleSets) > 0 and RulesetValidatorSelector.DefaultRuleSetName.lower() not in rule.RuleSets:
             return False
         return True
     

4e930b940244c3fa365c59d37b229b90bc9496f5
phzamora <p.hzamora@icloud.com>
2024-08-24 22:02:28 +0200

    update: enums.py

--- src/fluent_validation/enums.py
+++ src/fluent_validation/enums.py
@@ -4,3 +4,14 @@ from enum import Enum, auto
 class CascadeMode(Enum):
     Continue = auto()
     Stop = auto()
+
+
+class ApplyConditionTo(Enum):
+    AllValidators = auto()
+    CurrentValidator = auto()
+
+
+class Severity(Enum):
+    Error = auto()
+    Warning = auto()
+    Info = auto()

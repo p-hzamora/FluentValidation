45adcc2a7951a0fd9fca98e510649493f7030aba
p-hzamora <p.hzamora@icloud.com>
2024-08-26 12:07:27 +0200

    update: added 'IPropertyValidator_no_generic' interface

--- src/fluent_validation/validators/IpropertyValidator.py
+++ src/fluent_validation/validators/IpropertyValidator.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 
 from typing import TYPE_CHECKING
 
@@ -8,17 +7,20 @@ if TYPE_CHECKING:
     from ..IValidationContext import ValidationContext
 
 
-@dataclass
-class IPropertyValidator[T, TProperty](ABC):
+class IPropertyValidator_no_generic(ABC):
     @property
     @abstractmethod
-    def Name(self) -> str:
-        ...
+    def Name(self) -> str: ...
 
     @abstractmethod
-    def get_default_message_template(self, error_code: str) -> str:
-        ...
+    def get_default_message_template(self, error_code: str) -> str: ...
 
+
+class IPropertyValidator[T, TProperty](IPropertyValidator_no_generic):
     @abstractmethod
-    def is_valid(self, context: ValidationContext[T], value: TProperty) -> bool:
-        ...
+    def is_valid(self, context: ValidationContext[T], value: TProperty) -> bool: ...
+
+
+class IAsyncPropertyValidator[T, TProperty](IPropertyValidator_no_generic):
+    @abstractmethod
+    async def IsValidAsync(self, context: ValidationContext[T], value: TProperty) -> bool: ...

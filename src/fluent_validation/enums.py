39e2e372bc7b0c7da0da6c8edf60544f356a9aac
phzamora <p.hzamora@icloud.com>
2024-08-29 21:51:53 +0200

    update: StringComparer enums

--- src/fluent_validation/enums.py
+++ src/fluent_validation/enums.py
@@ -15,3 +15,9 @@ class Severity(Enum):
     Error = auto()
     Warning = auto()
     Info = auto()
+
+
+# COMMENT: Replicated StringComparer C# enum
+class StringComparer(Enum):
+    Ordinal = lambda x, y: x == y  # noqa: E731
+    OrdinalIgnoreCase = lambda x, y: x.lower() == y.lower()  # noqa: E731

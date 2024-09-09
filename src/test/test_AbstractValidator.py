c14899b07a771aa5da5e262aece45edd93f8b6f6
p-hzamora <p.hzamora@icloud.com>
2024-09-09 11:06:43 +0200

    test: fixed 'Validates_single_property_by_path'

--- src/test/test_AbstractValidator.py
+++ src/test/test_AbstractValidator.py
@@ -142,7 +142,7 @@ class AbstractValidatorTester(unittest.TestCase):
         self.validator.rule_for(lambda x: x.Address).set_validator(addressValidator)
         self.validator.rule_for(lambda x: x.Forename).not_null()
 
-        result = self.validator.validate(Person(Address=Address()), lambda v: v.IncludeProperties("Address[].Line1"))
+        result = self.validator.validate(Person(Address=Address()), lambda v: v.IncludeProperties("Address.Line1"))
         self.assertEqual(len(result.errors), 1)
         self.assertEqual(result.errors[0].PropertyName, "Address.Line1")
 

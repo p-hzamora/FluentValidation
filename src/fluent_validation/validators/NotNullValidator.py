4c7bfd282b954206d54e41aaaf32f30873c5b7a5
phzamora <p.hzamora@icloud.com>
2024-09-14 19:42:24 +0200

    fix: added 'INotNullValidator' interface

--- src/fluent_validation/validators/NotNullValidator.py
+++ src/fluent_validation/validators/NotNullValidator.py
@@ -1,9 +1,13 @@
 from typing import override
 from fluent_validation.IValidationContext import ValidationContext
 from fluent_validation.validators.PropertyValidator import PropertyValidator
+from fluent_validation.validators.IpropertyValidator import IPropertyValidator
+
+class INotNullValidator(IPropertyValidator): ...
 
 
-class NotNullValidator[T, TProperty](PropertyValidator):
+
+class NotNullValidator[T, TProperty](PropertyValidator[T, TProperty], INotNullValidator ):
     @override
     def is_valid(self, _: ValidationContext, value: TProperty) -> bool:
         return value is not None

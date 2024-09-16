3bdf40aaa4977b6330ea2072984c9bd145eb2d5c
p-hzamora <p.hzamora@icloud.com>
2024-09-16 15:50:04 +0200

    test: updated InlineValidator

--- src/fluent_validation/InlineValidator.py
+++ src/fluent_validation/InlineValidator.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Callable
 
 from .abstract_validator import AbstractValidator
@@ -5,11 +6,24 @@ from .syntax import IRuleBuilderOptions
 
 
 class InlineValidator[T](AbstractValidator[T]):
-    def __init__(self) -> None:
-        super().__init__()
+    """
+     Validator implementation that allows rules to be defined without inheriting from AbstractValidator.
+     
+     EXAMPLE
+     -
 
-    def Add[TProperty](
-        self,
-        ruleCreator: Callable[["InlineValidator"], IRuleBuilderOptions[T, TProperty]],
-    ):
-        ruleCreator(self)
+    @dataclass
+     public class Customer:
+       Id: int
+       Name: str
+    
+       public static readonly InlineValidator&lt;Customer&gt; Validator = new InlineValidator&lt;Customer&gt; {
+         v =&gt; v.RuleFor(x =&gt; x.Name).NotNull(),
+         v =&gt; v.RuleFor(x =&gt; x.Id).NotEqual(0),
+       }
+     }
+    """
+    def __init__[TProperty](self,*ruleCreator: Callable[[InlineValidator[T]], IRuleBuilderOptions[T, TProperty]]) -> None:
+        super().__init__()
+        for rule in ruleCreator:
+            rule(self)

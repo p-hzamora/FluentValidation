8e992d3ab15a8e10dd149e31e8554cc48c66a896
p-hzamora <p.hzamora@icloud.com>
2024-09-16 16:13:46 +0200

    fix: moved AbstractValidator in __init__.py

--- src/fluent_validation/__init__.py
+++ src/fluent_validation/__init__.py
@@ -1,4 +1,3 @@
-from fluent_validation.abstract_validator import AbstractValidator  # noqa: F401
 from fluent_validation.enums import (  # noqa: F401
     CascadeMode,
     ApplyConditionTo,
@@ -6,5 +5,6 @@ from fluent_validation.enums import (  # noqa: F401
     StringComparer,
 )
 from fluent_validation.IValidationContext import ValidationContext  # noqa: F401
+from fluent_validation.abstract_validator import AbstractValidator  # noqa: F401
 
 from fluent_validation.ValidatorOptions import ValidatorOptions  # noqa: F401

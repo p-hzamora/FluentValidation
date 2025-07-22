c822c64351f7022fb1c7d3d9670eddde5c4d280d
p-hzamora <p.hzamora@icloud.com>
2025-07-22 14:42:24 +0200

    chore: updated some __init__ files

--- src/fluent_validation/__init__.py
+++ src/fluent_validation/__init__.py
@@ -1,24 +1,24 @@
 # Enums
-from fluent_validation.enums import (  # noqa: F401
-    CascadeMode,
-    ApplyConditionTo,
-    Severity,
-    StringComparer,
-)
-from fluent_validation.IValidationContext import ValidationContext  # noqa: F401
-from fluent_validation.abstract_validator import AbstractValidator  # noqa: F401
-from fluent_validation.syntax import IRuleBuilder, IRuleBuilderOptions  # noqa: F401
+from fluent_validation.enums import CascadeMode as CascadeMode
+from fluent_validation.enums import ApplyConditionTo as ApplyConditionTo
+from fluent_validation.enums import Severity as Severity
+from fluent_validation.enums import StringComparer as StringComparer
+
+from fluent_validation.IValidationContext import ValidationContext as ValidationContext
+from fluent_validation.abstract_validator import AbstractValidator as AbstractValidator
+from fluent_validation.syntax import IRuleBuilder as IRuleBuilder
+from fluent_validation.syntax import IRuleBuilderOptions as IRuleBuilderOptions
 
 # Internal class
-from fluent_validation.internal.PropertyChain import PropertyChain  # noqa: F401
-from fluent_validation.internal.RuleSetValidatorSelector import RulesetValidatorSelector  # noqa: F401
+from fluent_validation.internal.PropertyChain import PropertyChain as PropertyChain
+from fluent_validation.internal.RuleSetValidatorSelector import RulesetValidatorSelector as RulesetValidatorSelector
 
 # Result class
-from fluent_validation.results.ValidationResult import ValidationResult  # noqa: F401
-from fluent_validation.results.ValidationFailure import ValidationFailure  # noqa: F401
+from fluent_validation.results.ValidationResult import ValidationResult as ValidationResult
+from fluent_validation.results.ValidationFailure import ValidationFailure as ValidationFailure
 
 # Custom Validation
-from fluent_validation.validators.PropertyValidator import PropertyValidator  # noqa: F401
+from fluent_validation.validators.PropertyValidator import PropertyValidator as PropertyValidator
 
 # Global class
-from fluent_validation.ValidatorOptions import ValidatorOptions  # noqa: F401
+from fluent_validation.ValidatorOptions import ValidatorOptions as ValidatorOptions

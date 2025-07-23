47b5c3ea1c100a20ba25ed1f7460b87040d7bf98
p-hzamora <p.hzamora@icloud.com>
2025-07-23 17:37:45 +0200

    fix: 'NotEmptyValidator' class

--- src/fluent_validation/validators/NotEmptyValidator.py
+++ src/fluent_validation/validators/NotEmptyValidator.py
@@ -1,7 +1,18 @@
+import datetime
 from typing import override, Iterable
 from fluent_validation.IValidationContext import ValidationContext
 from fluent_validation.validators.PropertyValidator import PropertyValidator
 from fluent_validation.validators.IpropertyValidator import IPropertyValidator
+
+
+def is_not_default(value):
+    if isinstance(value, int | float):
+        return value != 0
+
+    if isinstance(value, datetime.datetime):
+        # We assume that a minimun datetime means that the data is not valid, that is, it's empty
+        return value != datetime.datetime.min
+    return value is not None
 
 
 class INotEmptyValidator(IPropertyValidator): ...
@@ -13,11 +24,25 @@ class NotEmptyValidator[T, TProperty](PropertyValidator, INotEmptyValidator):
         if value is None:
             return False
 
-        if isinstance(value, str):
-            return not value.isspace() and value != ""
+        if isinstance(value, str) and value.strip() == "":
+            return False
 
         if isinstance(value, Iterable):
-            return len(value) > 0
+            if hasattr(value, "__len__"):
+                return len(value) > 0
+
+            if hasattr(value, "__iter__") and not isinstance(value, (str, bytes)):
+                try:
+                    iterator = iter(value)
+                    if next(iterator, None) is None:
+                        return False
+                    pass
+                except StopIteration:
+                    return False
+
+        # For primitive types, check against their default value
+        # Python does not have a generic default
+        return is_not_default(value)
 
     @override
     def get_default_message_template(self, error_code: str) -> str:

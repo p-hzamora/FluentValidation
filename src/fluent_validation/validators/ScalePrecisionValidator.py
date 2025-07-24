916707121f6302dfca1ef3d5088d49e49b862b8b
p-hzamora <p.hzamora@icloud.com>
2025-07-24 10:03:36 +0200

    chore: added License at the top of file

--- src/fluent_validation/validators/ScalePrecisionValidator.py
+++ src/fluent_validation/validators/ScalePrecisionValidator.py
@@ -1,3 +1,21 @@
+# region License
+# Copyright (c) .NET Foundation and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# The latest version of this file can be found at https://github.com/p-hzamora/FluentValidation
+# endregion
+
 # Attribution: This class was contributed to FluentValidation using code posted on StackOverflow by Jon Skeet
 # The original code can be found at https:#stackoverflow.com/a/764102
 # [p-hzamora] I've tried to adapt it with Decimal object in Python
